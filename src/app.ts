74bcd4ccdeea58444db043f5f58841135f065c63
Igor Samarin <samariniv@oblteh.ru>
2024-12-23 13:54:38 +0300

    Resolve cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import fastify from 'fastify';
+import fastifyCors from '@fastify/cors';
 import weatherRoutes from './routes/weatherRoutes';
 
 const buildApp = () => {
   const app = fastify({ logger: true });
+
+  app.register(fastifyCors, {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
 
   app.register(weatherRoutes, { prefix: '/api/weather' });
 
